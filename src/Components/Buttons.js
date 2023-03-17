2aa00eb28ea74930169a39c7d1da884965d403d1
Prakhar Tamta <prakhartamta@gmail.com>
2023-03-17 12:08:07 +0530

    disabled button

--- src/Components/Buttons.js
+++ src/Components/Buttons.js
@@ -4,7 +4,7 @@ class Buttons extends React.Component {
     render() {
         return (
             <div className="th-btn-container">
-                <button type='button' className="th-button" disabled={this.props.id <= 0} onClick={() => this.props.onNext(this.props.id - 12)}>Prev</button>
+                {this.props.id <= 0 ? <button type='button' className="th-disabled-button">Prev</button> : <button type='button' className="th-button" onClick={() => this.props.onNext(this.props.id - 12)}>Prev</button>}
                 <button type='button' className="th-button" onClick={() => this.props.onNext(this.props.id + 12)}>Next</button>
             </div>
         );
